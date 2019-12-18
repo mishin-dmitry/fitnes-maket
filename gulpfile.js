71d915a8a2d1c9aaa4a03f08638367e958830acb
mishindmitry95 <dima_mishin_95@mail.ru>
2019-12-18 07:22:10 +0300

    обновляет gulp сборку

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,109 @@
+"use strict";
+
 var gulp = require("gulp");
-var rename = require("gulp-rename");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
 var sass = require("gulp-sass");
-var autoprefixer = require("gulp-autoprefixer");
-var sourcemaps = require("gulp-sourcemaps");
-var browserSync = require("browser-sync").create();
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
 
-function css_style(done) {
-  gulp
-    .src("./scss/**/*")
-    .pipe(sourcemaps.init())
-    .pipe(
-      sass({
-        errorLogToConsole: true,
-        outputStyle: "compressed"
-      })
-    )
-    .on("error", console.error.bind(console))
-    .pipe(
-      autoprefixer({
-        browsers: ["last 2 versions"],
-        cascade: false
-      })
-    )
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest("./css/"))
-    .pipe(browserSync.stream());
+gulp.task("css", function() {
+  return gulp
+    .src("source/scss/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
 
-  done();
-}
-
-function watchFiles() {
-  gulp.watch("./scss/style.scss", css_style);
-  gulp.watch("./**/*", browserReload);
-}
-
-function browserReload(done) {
-  browserSync.reload();
-  done();
-}
-
-function sync(done) {
-  browserSync.init({
-    server: {
-      baseDir: "./",
-      port: 3000
-    }
+gulp.task("server", function() {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
   });
-  done();
-}
 
-gulp.task("default", gulp.parallel(watchFiles, sync));
-gulp.task(css_style);
-//gulp.series(функции) - для вызова нескольких функций
+  gulp.watch("source/scss/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("refresh", function(done) {
+  server.reload();
+  done();
+});
+
+gulp.task("images", function() {
+  return gulp
+    .src("source/img/**/*.{png,jpg,svg}")
+    .pipe(
+      imagemin([
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.svgo()
+      ])
+    )
+
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function() {
+  return gulp
+    .src("source/img/**/*.{png,jpg}")
+    .pipe(webp({ quality: 90 }))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function() {
+  return gulp
+    .src("source/img/{icon-*,htmlacademy*}.svg")
+    .pipe(svgstore({ inlineSvg: true }))
+    .pipe(rename("sprite_auto.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function() {
+  return gulp
+    .src("source/*.html")
+    .pipe(posthtml([include()]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("copy", function() {
+  return gulp
+    .src(
+      [
+        "source/fonts/**/*.{woff,woff2}",
+        "source/img/**",
+        "source/js/**",
+        "source//*.ico"
+      ],
+      {
+        base: "source"
+      }
+    )
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("start", gulp.series("build", "server"));
